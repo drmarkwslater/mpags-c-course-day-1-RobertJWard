de4f4588f5c7d170f286088630de9afa05a2a4bc
RobertJWard <robert.james.ward@cern.ch>
2018-10-05 16:13:26 +0100

    Transliterate program

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,44 +1,63 @@
-// mpags-cipher.cpp source file
-
 #include<iostream>
 #include<string>
 
-
 int main()
 {
-  // This is a comment
-  /* Multiline comment
-     still commented */
+  char inChar{'x'};
+  std::string message{""};
+  while(std::cin >> inChar)
+    {
+      //Loop until the user presses Enter then Ctrl+D
 
-  std::cout << "Hello World!" << std::endl;
-  
-  int a{1}; //Uniform initialisation c++11
+      //Take each letter from user input and in each case:
 
-  a = 12 + 91;
+      //  - Convert to upper case
+      inChar = toupper(inChar);
 
-  double b{91.67};
+      //  - Change numbers to words
+      switch (inChar)
+	{
+	case '0':
+	  message.append("ZERO");
+	  break;
+	case '1':
+	  message.append("ONE");
+	  break;
+	case '2':
+	  message.append("TWO");
+	  break;
+	case '3':
+	  message.append("THREE");
+	  break;
+	case '4':
+	  message.append("FOUR");
+	  break;
+	case '5':
+	  message.append("FIVE");
+	  break;
+	case '6':
+	  message.append("SIX");
+	  break;
+	case '7':
+	  message.append("SEVEN");
+	  break;
+	case '8':
+	  message.append("EIGHT");
+	  break;
+	case '9':
+	  message.append("NINE");
+	  break;
+	}
+      //  - Ignore any other (non-alpha) characters
+      if (!isalpha(inChar))
+	  continue;
+      //  - In each case, add result to a string variable
+      char* outChar{&inChar};
+      message.append(outChar);
+    }
 
-  b*=1.37;
+  //print out the new string
+  std::cout << message << std::endl; 
 
-  int c{b};
-
-  std::cout << "a = " << a << ", b = " << b << ", c = " << c << std::endl;
-  
-  // endl tells the program to flush the code there and then, /n lets the program print whenever it fancies.
-  // ' means char, " means string 
-
-  std::string message{"This is a string"};
-  std::cout << message << std::endl;
-
-  const double d{12.234};
-  std::cout << "d = " << d << std::endl;
-
-  const int e{12};
-  //e=123; //<-- this causes an error!
-  std::cout << "e = " << e << std::endl;
-  
-  //Declare everything as const unless you know it needs to be changed at some point
-
-  
   return 0;
 }
