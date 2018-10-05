61a6a3f3a96d515e7c44af49ff9633dfc245b84e
RobertJWard <robert.james.ward@cern.ch>
2018-10-05 14:10:02 +0100

    Hello world!

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,1 +1,16 @@
 // mpags-cipher.cpp source file
+
+#include<iostream>
+
+int main()
+{
+  // This is a comment
+  /* Multiline comment
+     still commented */
+
+  std::cout << "Hello World!" << std::endl;
+
+  // endl tells the program to flush the code there and then, /n lets the program print whenever it fancies.
+  
+  return 0;
+}
